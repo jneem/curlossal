6125b93503a963a231e6fa1e0a5524674e636471
Joe Neeman <joeneeman@gmail.com>
2024-03-06 20:27:31 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Basic rust dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, fenix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-toolchain = pkgs.fenix.latest.withComponents [
+          "cargo"
+          "clippy"
+          "rustc"
+          "rustfmt"
+          "rust-analyzer"
+          "rust-src"
+        ];
+
+        nickel-cursor = pkgs.rustPlatform.buildRustPackage {
+          pname = "nickel-cursor";
+          version = "0.1.0";
+
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            cargo-outdated
+            nickel
+            nls
+            rust-toolchain
+          ];
+        };
+
+        packages.nickel-cursor = nickel-cursor;
+      }
+    );
+}
